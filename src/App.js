b177c86792d4323ffecfc3315e75a8747b6569df
Andrew Truong <andrew.b.truong@accenture.com>
2020-12-04 13:44:14 +1100

    initialise + added canvas & tool bars

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ReactPainter } from 'react-painter';
 import './App.css';
 
+
+
+
+
 function App() {
+
+  const Drawable = () => (
+    <ReactPainter
+      width={300}
+      height={300}
+      onSave={blob => console.log(blob)}
+      render={({ triggerSave, canvas, setColor, setLineWidth, setLineJoin, setLineCap }) => (
+        <div>
+          <div>Awesome heading</div>
+          <select onChange={e => setLineCap(e.target.value)}>
+            <option value="round">round</option>
+            <option value="butt">butt</option>
+            <option value="square">square</option>
+          </select>
+          <select onChange={e => setLineJoin(e.target.value)}>
+            <option value="round">round</option>
+            <option value="bevel">bevel</option>
+            <option value="miter">miter</option>
+          </select>
+          <input type="color" onChange={e => setColor(e.target.value)} />
+          <input type="number" onChange={e => setLineWidth(e.target.value)} />
+          <div className="awesomeContainer">{canvas}</div>
+          <div>{canvas}</div>
+          <button onClick={triggerSave}>Save Canvas</button>
+          
+        </div>
+      )}
+    />
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    Drawable()
   );
 }
 
