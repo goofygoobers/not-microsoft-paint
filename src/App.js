e91d651385072fd0462dfa904814faaf499c274d
Andrew Truong <andrew.b.truong@accenture.com>
2020-12-10 14:04:23 +1100

    now using react-konva lib

--- src/App.js
+++ src/App.js
@@ -1,45 +1,79 @@
-import React from 'react';
-import { ReactPainter } from 'react-painter';
-import './App.css';
+import React from "react";
+import { render } from "react-dom";
+import { Stage, Layer, Line, Text } from "react-konva";
 
+const App = () => {
+  const [tool, setTool] = React.useState("pen");
+  const [lines, setLines] = React.useState([]);
+  const isDrawing = React.useRef(false);
 
+  const handleMouseDown = (e) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+  };
 
+  const handleMouseMove = (e) => {
+    // no drawing - skipping
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
 
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
+  };
 
-function App() {
-
-  const Drawable = () => (
-    <ReactPainter
-      width={300}
-      height={300}
-      onSave={blob => console.log(blob)}
-      render={({ triggerSave, canvas, setColor, setLineWidth, setLineJoin, setLineCap }) => (
-        <div>
-          <div>Awesome heading</div>
-          <select onChange={e => setLineCap(e.target.value)}>
-            <option value="round">round</option>
-            <option value="butt">butt</option>
-            <option value="square">square</option>
-          </select>
-          <select onChange={e => setLineJoin(e.target.value)}>
-            <option value="round">round</option>
-            <option value="bevel">bevel</option>
-            <option value="miter">miter</option>
-          </select>
-          <input type="color" onChange={e => setColor(e.target.value)} />
-          <input type="number" onChange={e => setLineWidth(e.target.value)} />
-          <div className="awesomeContainer">{canvas}</div>
-          <div>{canvas}</div>
-          <button onClick={triggerSave}>Save Canvas</button>
-          
-        </div>
-      )}
-    />
-  );
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
 
   return (
-    Drawable()
+    <div>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}
+      >
+        <Layer>
+          <Text text="Just start drawing" x={5} y={30} />
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="blue"
+              strokeWidth={5}
+              tension={0.5}
+              lineCap="round"
+              globalCompositeOperation={
+                line.tool === "eraser" ? "destination-out" : "source-over"
+              }
+            />
+          ))}
+        </Layer>
+      </Stage>
+      <select
+        value={tool}
+        onChange={(e) => {
+          setTool(e.target.value);
+        }}
+      >
+        <option value="pen">Pen</option>
+        <option value="eraser">Eraser</option>
+      </select>
+    </div>
   );
-}
+};
+
+render(<App />, document.getElementById("root"));
+
+
 
 export default App;
