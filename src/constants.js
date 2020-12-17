0893a1c6a88ac744a1c278b99dbaab70bb994029
Andrew Truong <andrew.b.truong@accenture.com>
2020-12-17 21:17:33 +1100

    added sidebar with shapes

---
+++ src/constants.js
@@ -0,0 +1,36 @@
+export const SHAPE_TYPES = {
+  RECT: "rect",
+  CIRCLE: "circle",
+};
+
+export const DEFAULTS = {
+  RECT: {
+    STROKE: "#000000",
+    FILL: "#ffffff",
+    WIDTH: 150,
+    HEIGHT: 100,
+    ROTATION: 0,
+  },
+  CIRCLE: {
+    STROKE: "#000000",
+    FILL: "#ffffff",
+    RADIUS: 50,
+  },
+  TRIANGLE: {
+    STROKE: "#000000",
+    FILL: "#ffffff"
+  }
+};
+
+export const LIMITS = {
+  RECT: {
+    MAX: 1000,
+    MIN: 10,
+  },
+  CIRCLE: {
+    MAX: 500,
+    MIN: 5,
+  },
+};
+
+export const DRAG_DATA_KEY = "__drag_data_payload__";
